5d82a6a17aa806c39ece6b066ac40fd9a3212639
Aww <127321461+awesomeday3@users.noreply.github.com>
2023-03-29 19:26:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -598,7 +598,7 @@ app.post("/getcharacters", jsonParser, function(request, response){
 app.post("/getbackgrounds", jsonParser, function(request, response){
     var images = getImages("public/backgrounds");
     if(is_colab === true){
-        images = ['tavern.png', '1.png', '2.png', '3.png', '4.png'];
+        images = ['1.png', '2.png', '3.png', '4.png'];
     }
     response.send(JSON.stringify(images));
     
